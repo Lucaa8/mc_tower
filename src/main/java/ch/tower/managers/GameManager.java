15fc1b0e761817714b994e4ec7505d3d7b4e615b
m-jpeg <m.jpeg@outlook.com>
2023-02-13 17:05:49 +0100

    Add GameManager states changements

--- src/main/java/ch/tower/managers/GameManager.java
+++ src/main/java/ch/tower/managers/GameManager.java
@@ -1,13 +1,31 @@
 package ch.tower.managers;
 
 import ch.tower.Main;
+import ch.tower.events.EndEvents;
 import ch.tower.events.GameEvents;
+import ch.tower.events.WaitEvents;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+
+import java.util.logging.Handler;
 
 public class GameManager {
 
     enum GameState
     {
-        WAIT, GAME, END;
+        WAIT(WaitEvents.getInstance()),//
+        GAME(GameEvents.getInstance()),//
+        END(EndEvents.getInstance());
+
+        private Listener listener;
+        private GameState(Listener listener)
+        {
+            this.listener = listener;
+        }
+        public Listener getListener()
+        {
+            return listener;
+        }
     }
 
     private GameState state;
@@ -31,12 +49,10 @@ public class GameManager {
     public void setState(GameState state)
     {
         this.state = state;
-        //HandlerList.unregisterAll();
-        if(state == GameState.GAME)
-        {
-            Main.getInstance().getServer().getPluginManager().registerEvents(new GameEvents(), Main.getInstance());
-        }
-        //maybe register WaitEvents if state == GameState.WAIT, etc...
+        HandlerList.unregisterAll(EndEvents.getInstance());
+        HandlerList.unregisterAll(GameEvents.getInstance());
+        HandlerList.unregisterAll(WaitEvents.getInstance());
+        Main.getInstance().getServer().getPluginManager().registerEvents(this.state.getListener(), Main.getInstance());
     }
 
 }
