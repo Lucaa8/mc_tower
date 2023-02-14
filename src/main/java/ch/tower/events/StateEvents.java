437f9ee23b197b431eebc0176f7031d141d3ec40
m-jpeg <m.jpeg@outlook.com>
2023-02-14 09:33:42 +0100

    Interface StateEvents extends Listener

--- src/main/java/ch/tower/events/StateEvents.java
+++ src/main/java/ch/tower/events/StateEvents.java
@@ -1,6 +1,8 @@
 package ch.tower.events;
 
-public interface StateEvents
+import org.bukkit.event.Listener;
+
+public interface StateEvents extends Listener
 {
     public void onStateBegin();
     public void onStateLeave();
