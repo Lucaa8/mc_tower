b39ac91d729b26bfdc91ab1992824c03b4e309c6
Luca <47627900+Lucaa8@users.noreply.github.com>
2023-03-05 00:33:01 +0100

    Rollback (not changed from main)

--- src/main/java/ch/tower/events/GlobalEvents.java
+++ src/main/java/ch/tower/events/GlobalEvents.java
@@ -1,13 +1,7 @@
 package ch.tower.events;
 
-import ch.luca008.SpigotApi.SpigotApi;
 import ch.tower.Main;
-import ch.tower.utils.NPC.NPCCreator;
-import ch.tower.utils.Packets.EntityPackets;
-import ch.tower.utils.Packets.SpigotPlayer;
 import ch.tower.utils.Scoreboard.PlayerBoard;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -20,10 +14,6 @@ public class GlobalEvents implements Listener {
     public void helloNewPlayer(PlayerJoinEvent e)
     {
         e.getPlayer().sendMessage("Welcome. The Tower Plugin is in function.");
-        final Location spawnNpc = new Location(Bukkit.getWorld("Spawn"), 6, 50, 0);
-        Bukkit.getScheduler().runTaskLater(Main.getInstance(), ()->
-                new NPCCreator().registerNPC("testNPC", "Shop", "Lord_Tigrou", spawnNpc),
-                20L);
     }
 
     @EventHandler
