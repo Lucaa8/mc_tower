8751c8580ef25e0047fa700092514637deb09ff7
Luca <47627900+Lucaa8@users.noreply.github.com>
2023-05-01 21:35:03 +0200

    Contains all potions. Some potions are normal like speed, heal, etc.. But there are Beer potion (custom potion of default tower map) and the luck potion. This potion gives a random effet (maybe 2 more hearts util death) or a random item. (not implemented yet)

---
+++ src/main/java/ch/tower/shop/categoryMenus/PotionMenu.java
@@ -0,0 +1,40 @@
+package ch.tower.shop.categoryMenus;
+
+import ch.luca008.SpigotApi.Api.JSONApi;
+import ch.tower.TowerPlayer;
+import ch.tower.shop.ShopMenu;
+import ch.tower.utils.items.Item;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.Inventory;
+
+public class PotionMenu extends ShopMenu {
+
+    public PotionMenu(String id, JSONApi.JSONReader json)
+    {
+        super(id, json);
+    }
+
+    @Override
+    public Inventory createInventory(TowerPlayer player)
+    {
+        Inventory inv = super.createInventory(player);
+        for(Item i : super.content)
+        {
+            inv.setItem(i.getSlot(), prepareItem(i, true));
+        }
+        return inv;
+    }
+
+    @Override
+    public double clicked(TowerPlayer player, Item item, ClickType click)
+    {
+        double price = super.clicked(player, item, click);
+        if(price >= 0.0)
+        {
+            player.takeMoney(price);
+            item.giveOrDrop(player.asPlayer(), item.getCount());
+        }
+        return -1.0;
+    }
+
+}
