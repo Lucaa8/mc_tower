07de68a7e6e4645ee418fe76d7809074609038f8
Luca <47627900+Lucaa8@users.noreply.github.com>
2023-05-01 21:30:06 +0200

    Added the whole shop configuration into shop.json and added Shop.java to read and put this configuration into the ram on start. Contains a method to create the shop inventory with default glass/door. Contains a method called when a player click into a shop inventory.

---
+++ src/main/java/ch/tower/shop/Shop.java
@@ -0,0 +1,39 @@
+package ch.tower.shop;
+
+import ch.luca008.SpigotApi.Api.JSONApi;
+import ch.luca008.SpigotApi.SpigotApi;
+import ch.tower.TowerPlayer;
+import ch.tower.shop.categoryMenus.BlocksMenu;
+import ch.tower.utils.items.Item;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.Inventory;
+import org.json.simple.JSONObject;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface Shop {
+    Inventory createInventory(TowerPlayer player);
+
+    double clicked(TowerPlayer player, Item item, ClickType click);
+
+    static Shop loadShop(JSONObject json)
+    {
+        try {
+            String shop = (String)json.get("Id");
+            if(shop!=null&&!shop.isEmpty()){
+                shop = (shop.contains("_") ? shop.split("_")[1] : shop)
+                        .transform(s -> s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase()) + "Menu";
+                JSONObject shopContent = new JSONObject();
+                shopContent.put("Shop", json.get("Shop"));
+                shopContent.put("Prices", json.get("Prices"));
+                return (Shop)Class.forName(BlocksMenu.class.getPackage().getName()+"."+shop)
+                        .getConstructor(String.class, JSONApi.JSONReader.class)
+                        .newInstance((String)json.get("Id"), SpigotApi.getJSONApi().getReader(shopContent));
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            System.err.println("Can't load Shop with JSON: \n" + JSONApi.prettyJson(json));
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
