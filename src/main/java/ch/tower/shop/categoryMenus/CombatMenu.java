e2ef6bd118e6d525baa9b7de8d84d7657216fc42
Luca <47627900+Lucaa8@users.noreply.github.com>
2023-05-01 21:36:56 +0200

    This shop contains various pvp help items like gapple, water bucket. And the two custom items of the default tower map (Rod of fire and Harmless feather). The harmless feather reduce fall damage when in (off-)hand (not implemented yet)

---
+++ src/main/java/ch/tower/shop/categoryMenus/CombatMenu.java
@@ -0,0 +1,71 @@
+package ch.tower.shop.categoryMenus;
+
+import ch.luca008.SpigotApi.Api.JSONApi;
+import ch.tower.TowerPlayer;
+import ch.tower.shop.ShopMenu;
+import ch.tower.utils.items.Item;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
+public class CombatMenu extends ShopMenu {
+
+    public CombatMenu(String id, JSONApi.JSONReader json)
+    {
+        super(id, json);
+    }
+
+    @Override
+    public Inventory createInventory(TowerPlayer player)
+    {
+        Inventory inv = super.createInventory(player);
+        for(Item i : super.content)
+        {
+            inv.setItem(i.getSlot(), prepareItem(i, true));
+        }
+        return inv;
+    }
+
+    @Override
+    public double clicked(TowerPlayer player, Item item, ClickType click)
+    {
+        double price = super.clicked(player, item, click);
+        if(price >= 0.0)
+        {
+            player.takeMoney(price);
+            ItemStack is = prepareItem(item, false);
+            removeMultiplesLF(is);
+            giveItem(player.asPlayer(), is);
+        }
+        return -1.0;
+    }
+
+    private void removeMultiplesLF(ItemStack is)
+    {
+        if(is != null && is.hasItemMeta() && is.getItemMeta().hasLore())
+        {
+            List<String> lore = is.getItemMeta().getLore();
+            String last = null;
+            for(int i=0;i<lore.size();i++)
+            {
+                if(last==null)
+                    last = lore.get(i);
+                else
+                {
+                    if(last.equals("") && lore.get(i).equals(""))
+                    {
+                        lore.remove(i);
+                        break;
+                    }
+                    last = lore.get(i);
+                }
+            }
+            ItemMeta im = is.getItemMeta();
+            im.setLore(lore);
+            is.setItemMeta(im);
+        }
+    }
+}
