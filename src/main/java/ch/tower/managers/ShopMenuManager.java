4193cd8668a0fe030b22489274226c411765b520
Luca <47627900+Lucaa8@users.noreply.github.com>
2023-05-01 21:40:42 +0200

    This manager stores all the current actives shop

---
+++ src/main/java/ch/tower/managers/ShopMenuManager.java
@@ -0,0 +1,85 @@
+package ch.tower.managers;
+
+import ch.luca008.SpigotApi.Api.JSONApi;
+import ch.luca008.SpigotApi.SpigotApi;
+import ch.tower.Main;
+import ch.tower.TowerPlayer;
+import ch.tower.shop.Shop;
+import ch.tower.shop.ShopMenu;
+import ch.tower.utils.NPC.NPCCreator;
+import ch.tower.utils.items.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.json.simple.JSONObject;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+
+public class ShopMenuManager {
+
+    //https://pastebin.com/5rQsE69L
+    public static final File SHOP_FILE = new File(Main.getInstance().getDataFolder(), "shop.json");
+
+    private final List<ShopMenu> shops;
+
+    public ShopMenuManager()
+    {
+        JSONApi.JSONReader r = SpigotApi.getJSONApi().readerFromFile(SHOP_FILE);
+        this.shops = ((List<Object>) r.getArray("Shop-Items")).stream()
+                .filter(JSONObject.class::isInstance).map(JSONObject.class::cast)
+                .peek(j->j.put("Prices", r.getJson("Shop-Prices").getArray((String) j.get("Id"))))
+                .map(Shop::loadShop).filter(Objects::nonNull)
+                .filter(ShopMenu.class::isInstance).map(ShopMenu.class::cast)
+                .toList();
+    }
+
+    public void openShop(NPCCreator.NPC npc, Player player)
+    {
+        String shop = npc.getName().toLowerCase();
+        if(shop.endsWith("§f"))
+            shop = shop.substring(0, shop.length()-4);
+        openShop(shop, TowerPlayer.getPlayer(player));
+    }
+
+    public void openShop(String shop, TowerPlayer player)
+    {
+        if(player == null || player.getTeam()==TeamsManager.PlayerTeam.SPECTATOR)
+            return;
+        ShopMenu s = getShop(shop);
+        if(s!=null)
+        {
+            player.asPlayer().openInventory(s.createInventory(player));
+        }
+    }
+
+    @Nullable
+    public ShopMenu getShop(String id)
+    {
+        for(ShopMenu s : shops)
+        {
+            if(s.getId().equals(id))
+            {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public void invClickManager(Player player, String shop, String item, ClickType click){
+        for(ShopMenu sm : this.shops)
+        {
+            if(sm.getId().equals(shop))
+            {
+                Item clicked = sm.getItem(item);
+                if(clicked != null || item.equals("back"))
+                {
+                    sm.clicked(TowerPlayer.getPlayer(player), clicked, click);
+                    break;
+                }
+            }
+        }
+    }
+
+}
